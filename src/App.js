795a6d9c2de6232b680e7805805362b2266ce7d4
baoloc008 <baoloc008@gmail.com>
2020-06-29 14:23:25 +0700

    add useCallback

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { increment } from "./action";
+import { useCallback } from "react";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const increaseNumber = useCallback(() => {
     setTimeout(() => dispatch(increment()), 1000);
-  }, [count]);
+  }, [dispatch]);
+
+  useEffect(() => {
+    increaseNumber();
+  }, [count, increaseNumber]);
 
   return (
     <div className="App">
       <h1>Counter</h1>
-      <h3>{count}</h3>
+      <h2>{count}</h2>
     </div>
   );
 }
 
 export default App;
+// eslint-disable-next-line react-hooks/exhaustive-deps,
